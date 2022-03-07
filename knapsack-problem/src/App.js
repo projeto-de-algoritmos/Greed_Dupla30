5375cb6c2abe3391c52e723d3bc29675a27bc48d
Nícalo Ribeiro <nicalo63@gmail.com>
2022-03-06 22:39:19 -0300

    Correcoes gramaticais

--- knapsack-problem/src/App.js
+++ knapsack-problem/src/App.js
@@ -33,14 +33,14 @@ function App() {
     }, [selectedItems])
 
     const handleSelectItem = (item) => {
-        const arrayFiltered = items.filter(el => el.name != item.name)
+        const arrayFiltered = items.filter(el => el.name !== item.name)
         setSelectedItems((prevState) => [...prevState, item])
         setItems(arrayFiltered)
     }
 
     const handleRemoveItem = (item) => {
-        if(showSolution) return
-        const arrayFiltered = selectedItems.filter(el => el.name != item.name)
+        if (showSolution) return
+        const arrayFiltered = selectedItems.filter(el => el.name !== item.name)
         setItems((prevState) => [...prevState, item])
         setSelectedItems(arrayFiltered)
     }
@@ -81,19 +81,19 @@ function App() {
                     <div className='merchant'>
                         <img className='merchantImg' src='merchant.png' alt="Merchant" />
                         <p style={{ fontSize: '15px', color: 'white' }}>Olá! Seja bem-vindo ao nosso trabalho de projetos de algoritmos!<br /><br />
-                            Hoje você será um mercador que está acampado próximo a uma vila. Você possui mercadorias e deve transportar até a vila o que te dará mais lucro, mas respeitando o peso máximo que você consegue carregar. <br /><br />
-                            Clique nas mercadorias ao lado para ir adicionando à sua carroça e, quando achar que é o suficiente, faça a viagem.<br/>
-                            Abaixo de cada item você tem o seu peso (P) e o valor (V) e, ao passar o mouse sobre eles, haverá mais informações.<br/>
-                            Ao final, nós te diremos quais items vão te dar o maior lucro. Será que você é um bom mercador?</p>
+                            Você é um mercador que está acampado próximo a uma vila e, em seu acampamento, estão mercadorias que devem ser escolhidas e transportadas até lá. Dependendo das mercadorias que escolher, seu lucro será maior, porém tome cuidado para não exceder o limite de peso que você consegue carregar.<br /><br />
+                            Clique nas mercadorias ao lado para ir adicionando à sua carroça. Quando achar que é o suficiente, faça sua viagem.<br />
+                            Abaixo de cada item você tem o respectivo peso (P) e valor (V) e, ao passar o mouse sobre eles, haverá mais informações.<br />
+                            Ao final, nós lhe diremos quais items vão lhe dar o maior lucro. Será que você é um bom mercador?</p>
                     </div>
                 </div>
                 <div className='rightArea'>
                     {
-                        
+
                         <ItemArea>
                             <div style={{ textAlign: 'center', width: '100%' }}>
                                 <p>Essas são as mercadorias de hoje.</p>
-                                <p>Você irá conseguir carregar {bagWeight}kg.</p>
+                                <p>Você consegue carregar {bagWeight}kg.</p>
                             </div>
                             {
                                 items.length > 0 ?
@@ -101,7 +101,9 @@ function App() {
                                         <Item item={el} key={Math.random()} selectItem={handleSelectItem} />
                                     ))
                                     :
-                                    <p>Sem items...</p>
+                                    <div style={{ textAlign: 'center', width: '100%' }}>
+                                        <p>Sem mercadorias...</p>
+                                    </div>
                             }
                         </ItemArea>
                     }
@@ -131,14 +133,14 @@ function App() {
                             <div style={{ textAlign: 'center', width: '100%' }}>
                                 <p>Solução: </p>
                             </div>
-                            {solution[1].map(el => <Item item={el} key={el.name} selectItem={() => {}} />)}
+                            {solution[1].map(el => <Item item={el} key={el.name} selectItem={() => { }} />)}
                             <div style={{ textAlign: 'center', width: '100%' }}>
                                 <p>Valor total: {solution[0]}</p>
                             </div>
                         </ItemArea>
                     }
-                    <div style={{ display: 'flex'}}>
-                        {!showSolution && 
+                    <div style={{ display: 'flex' }}>
+                        {!showSolution &&
                             <button className='buttonClass' type='button' onClick={finishAttempt}>Finalizar</button>
                         }
                         <button className='buttonClass' type='button' onClick={resetAttempt}>Reiniciar</button>
